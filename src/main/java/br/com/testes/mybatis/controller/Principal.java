ec1999da6266803c94750899866e6b7b61cf5c80
Ramon Mendes de Souza <ramon@ciandt.com>
2014-05-03 22:27:13 -0300

    Alterações para validação do batis sozinho

--- src/main/java/br/com/testes/mybatis/controller/Principal.java
+++ src/main/java/br/com/testes/mybatis/controller/Principal.java
@@ -2,7 +2,6 @@ package br.com.testes.mybatis.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
@@ -12,25 +11,26 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import br.com.testes.mybatis.dao.ClienteMapper;
 import br.com.testes.mybatis.model.Cliente;
+import br.com.testes.mybatis.model.ClienteExample;
 
 public class Principal {
 	public static void main(String[] args) {
 		String resource = "br/com/testes/mybatis/controller/mybatis-config.xml";
 		InputStream inputStream;
 		try {
+			
 			inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+			SqlSessionFactory sqlSessionFactory = builder.build(inputStream);
 			sqlSessionFactory.getConfiguration().addMapper(ClienteMapper.class);
 			SqlSession sqlSession = sqlSessionFactory.openSession();
 			ClienteMapper mapper = sqlSession.getMapper(ClienteMapper.class);
-			List<Cliente> allRecords = new ArrayList<Cliente>();
-			allRecords.addAll(mapper.findAll());
-			for (Cliente cliente : allRecords) {
+			List<Cliente> selectByExample = mapper.selectByExample(new ClienteExample());
+			for (Cliente cliente : selectByExample) {
 				System.out.println(cliente.getNome());
 			}
 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
